852d490c8dac2419c0bb87249c39cfbd2f3138ad
Joice Gomes <devjoicegomes@gmail.com>
2023-03-26 23:53:29 -0300

    feat: create products service

---
+++ src/app/products.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { IProduct, products } from './products';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  products: IProduct[] = products;
+
+  constructor() { }
+
+  getAll(): IProduct[] {
+    return this.products;
+  }
+
+  getOne(productId: number):IProduct | undefined {
+    return this.products.find(product => product.id == productId);
+  }
+}
