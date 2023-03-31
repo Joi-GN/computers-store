c6cfdbcb27502938e05007f94b9381181439b068
Joice Gomes <devjoicegomes@gmail.com>
2023-03-31 00:43:29 -0300

    feat: create search bar component

---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent {
+  input = "";
+
+  constructor(private router: Router) { }
+
+  search(): void {
+    if(this.input) {
+      this.router.navigate(["products"], { queryParams: { name: this.input }});
+    } else {
+      this.router.navigate([""])
+    }
+  }
+}
