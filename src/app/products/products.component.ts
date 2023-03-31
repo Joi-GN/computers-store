b4cea7c9ad882119accd5c87cccd2436b4f8e4ed
Joice Gomes <devjoicegomes@gmail.com>
2023-03-31 00:45:00 -0300

    feat: display search results on products page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { IProduct } from '../products';
 import { ProductsService } from '../products.service';
 
@@ -10,9 +11,22 @@ import { ProductsService } from '../products.service';
 export class ProductsComponent implements OnInit {
   products : IProduct[] | undefined;
 
-  constructor (private productsService : ProductsService) {}
+  constructor (
+    private productsService : ProductsService,
+    private route: ActivatedRoute,
+    ) {}
 
   ngOnInit(): void {
+    this.route.queryParamMap.subscribe(param => {
+      const name = param.get("name")?.toLowerCase();
       this.products = this.productsService.getAll();
+
+      if (name) {
+        this.products = this.products.filter(product => product.name.toLowerCase().includes(name))
+      } else {
+        this.products = this.productsService.getAll();
+      }
+    })
+
   }
 }
